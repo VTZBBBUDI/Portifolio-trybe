4d24d2e7b268a24c4509a5087e23f96cda3b4f67
VTZBBBUDI <artyarthur3@gmail.com>
2023-05-11 15:45:10 -0300

    bpbp

---
+++ fundamentos/secao-03-indruducao-a-javascript/dia-02-javascript-array-e-loop/for-of.js
@@ -0,0 +1,44 @@
+let numeros = [1,2,3,4,5];
+for(let numero of numeros) {
+  console.log(numero);
+}
+
+// resultado: 
+//1
+//2
+//3
+//4
+//5
+
+console.log(        )
+
+let word = 'Hello';
+for (let letter of word) {
+  console.log(letter);
+}
+// resultado:
+// "H"
+// "e"
+// "l"
+// "l"
+// "o"
+
+console.log(        )
+
+let arrOfNumbers = [10, 20, 30];
+for (let soma of arrOfNumbers) {
+  soma += 1;
+  console.log(soma);
+}
+// 11
+// 21
+// 31
+
+//EXERCICIOS
+
+console.log(        )
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let bola of names) {
+    console.log(bola);
+}
