b361cf945402e6b600e9d230e41e6d4aab2afe49
VTZBBBUDI <artyarthur3@gmail.com>
2023-05-11 11:00:18 -0300

    blubul

--- fundamentos/secao-03-indruducao-a-javascript/dia-02-javascript-array-e-loop/arrays(lista).js
+++ fundamentos/secao-03-indruducao-a-javascript/dia-02-javascript-array-e-loop/arrays(lista).js
@@ -30,3 +30,31 @@ console.log(       )
 
 console.log(macaco.sort())
 
+console.log(       )
+
+//*********************************PARA EXIBIR OS ITENS FORA DE COLCHETES USA A OPERACAO*********
+
+for (let index = 0; index < macaco.length; index += 1) {
+    console.log(macaco[index]);
+}
+
+console.log(       )
+
+//*************************************** POP E SHIFT ***************************
+
+//O método .pop() permite remover o último item do array.
+//Mas, se for necessário remover o primeiro item da lista, é possível usar o .shift(). 
+
+let listao = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+listao.pop();  // remove a última tarefa
+
+console.log(listao);
+
+console.log(       )
+
+let boll = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+boll.shift();
+
+console.log(boll); // remove a primeira tarefa
+
+console.log(       )
