97b17c9fc9b29619774dbc1de94abcc7d106d23e
VTZBBBUDI <artyarthur3@gmail.com>
2023-05-11 15:05:42 -0300

    blola

--- fundamentos/secao-03-indruducao-a-javascript/dia-02-javascript-array-e-loop/loop for.js
+++ fundamentos/secao-03-indruducao-a-javascript/dia-02-javascript-array-e-loop/loop for.js
@@ -6,3 +6,24 @@ let numero = 4;
 for (let contador = 1; contador <=10; contador += 1) {
     console.log(numero * contador);
 }
+
+console.log(       )
+
+//PARA STRINGS
+
+let strings = ['paula', 'luis', 'lambisgoia', 'rafael', 'lambebola'];
+
+for (let indice = 0; indice < 5; indice += 1) {
+    console.log(strings[indice]);
+}
+
+//DE FORMA OTIMIZADA
+
+console.log(       )
+
+let nome = ['paula', 'luis', 'lambisgoia', 'rafael', 'lambebola']
+nome.push('lindinho')
+
+for (let index =0; index < nome.length; index += 1) {
+    console.log(nome[index])
+}
