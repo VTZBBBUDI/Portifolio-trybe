c56f9396e270997807ad00b1ff5801f43a951a99
VTZBBBUDI <artyarthur3@gmail.com>
2023-05-11 16:18:36 -0300

    lmam

---
+++ fundamentos/secao-03-indruducao-a-javascript/dia-02-javascript-array-e-loop/algoritimos.js
@@ -0,0 +1,14 @@
+let fruits = [3, 4, 10, 1, 12];
+let soma = 0;
+
+for (let loop = 0; loop < fruits.length; loop += 1) {
+    soma += fruits[loop];
+};
+
+if (soma > 15) {
+    console.log(soma)
+} else {
+    console.log('bosta');
+}
+
+console.log(               );
