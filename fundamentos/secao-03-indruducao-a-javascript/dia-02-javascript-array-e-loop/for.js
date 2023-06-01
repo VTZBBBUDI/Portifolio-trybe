b1f9c359be0ccff0280896f8684f0ffaec5b945d
VTZBBBUDI <artyarthur3@gmail.com>
2023-06-01 16:31:35 -0300

    for-01-06-23

---
+++ fundamentos/secao-03-indruducao-a-javascript/dia-02-javascript-array-e-loop/for.js
@@ -0,0 +1,16 @@
+const laps = 68;
+
+for (let index = 1; index <= laps; index += 1) {
+
+  console.log(`Volta ${index} completada!`);
+
+}
+console.log('Acabou a corrida!');
+
+//const shopping = ['sabao', 'macarrao em po', 'leite densado', 'refri', 'fruta', 'suco de fruta'];
+
+//for (inicialização; condição; atualização) {
+  // console.log(shopping[index]);
+  //console.log(index);
+  // console.log(`${index} - ${shopping[index]}`);
+//};
