ae6c05680be549be133651ee4a8a8df067582a91
VTZBBBUDI <artyarthur3@gmail.com>
2023-06-21 15:39:50 -0300

    intro

---
+++ front-end/secao-01-indroducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/indrodução.js
@@ -0,0 +1,37 @@
+/*
+ Quando acessamos uma página na internet e temos acesso a vários botões,
+ barra de navegação, rodapé, cabeçalho e outros elementos visuais,
+ provavelmente isso foi implementado por uma pessoa desenvolvedora Front-end.
+*/
+/*
+ O desenvolvimento Front-end é o responsável por toda a parte gráfica das aplicações.
+ Não apenas aos elementos visuais como os citados acima, mas também pelas interações 
+ da pessoa usuária com a aplicação, seu layout, e até mesmo como ela irá se comportar 
+ em diferentes dispositivos, como telas de computadores, smartphones, 
+ tablets e até mesmo relógios inteligentes (smartwatches).
+*/
+/*
+ O que vamos aprender no módulo de Front-end
+  • Configuração de ambiente e javascript assíncrono
+  • React com TypeScript
+      • Fundamentos
+      • Hooks
+      • Estado, Props e Ciclo de Vida
+      • Formulários
+      • React Router
+      • Context API
+  • Metodologias Ágeis
+  • Testes Automatizados com React Testing Library
+  • Gerenciamento de Estado Global
+*/
+/*
+ Você será capaz de :
+  • Iniciar um projeto com NPM;
+  • Configurar pastas;
+  • Iniciar controle de versionamento com Git;
+  • Instalar bibliotecas externas;
+  • Instalar um Bundler e Web Server (Vite);
+  • Utilizar NPM, NPX e NPM Scripts;
+  • Configurar ESLint no projeto;
+  • Utilizar CSS no seu projeto.
+*/
